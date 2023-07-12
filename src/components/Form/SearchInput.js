f8d7884ee21d81a5366b67ea17d975c5c2ad8283
Harshkumar12014013 <101981203+Harshkumar12014013@users.noreply.github.com>
2023-07-12 14:05:48 +0530

    link change

--- src/components/Form/SearchInput.js
+++ src/components/Form/SearchInput.js
@@ -10,7 +10,7 @@ const handleSubmit = async (e) => {
         e.preventDefault();
         try {
           const { data } = await axios.get(
-            `/api/v1/product/search/${values.keyword}`
+            `https://ecommercestorebackend.onrender.com/api/v1/product/search/${values.keyword}`
           );
           setValues({ ...values, results: data });
           navigate("/search");
--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -49,7 +49,7 @@ const CartPage = () => {
 //get payment gateway token
 const getToken = async () => {
   try {
-    const { data } = await axios.get("/api/v1/product/braintree/token");
+    const { data } = await axios.get("https://ecommercestorebackend.onrender.com/api/v1/product/braintree/token");
     setClientToken(data?.clientToken);
   } catch (error) {
   }
@@ -63,7 +63,7 @@ useEffect(() => {
   try {
     setLoading(true);
     const { nonce } = await instance.requestPaymentMethod();
-    const { data } = await axios.post("/api/v1/product/braintree/payment", {
+    const { data } = await axios.post("https://ecommercestorebackend.onrender.com/api/v1/product/braintree/payment", {
       nonce,
       cart,
     });
@@ -103,7 +103,7 @@ useEffect(() => {
                 <div className="row card flex-row" key={p._id}>
                   <div className="col-md-4">
                     <img
-                      src={`/api/v1/product/product-photo/${p._id}`}
+                      src={`https://ecommercestorebackend.onrender.com/api/v1/product/product-photo/${p._id}`}
                       className="card-img-top"
                       alt={p.name}
                       width="100%"
